78ff48fee7c27108a8c5642dcc51d81cbbf6c1bb
Gronnfrosk <Hanna.Fjeldsaa@stud.noroff.no>
2023-02-28 21:08:16 +0100

    User can view a post content feed on home page and profile page

--- javascript/index.mjs
+++ javascript/index.mjs
@@ -1,6 +1,8 @@
 import { setRegisterFormListener } from "./modules/register.mjs";
 import { setLoginFormListener } from "./modules/login.mjs";
-//import * as post from "./api/posts/index.mjs";
+
+import * as templates from "./templates/index.mjs";
+import * as postMethods from "./api/posts/index.mjs";
 
 const path = location.pathname;
 
@@ -10,8 +12,31 @@ if (path === "/html/login.html") {
 	setRegisterFormListener();
 }
 
-//post.cratePost();
-//post.deletePost();
-//post.updatePost();
-//post.getPost();
+async function testTemplate() {
+	const posts = await postMethods.getPosts();
+	const container = document.querySelector("#allPosts");
+	templates.renderPostTemplates(posts, container);
+}
+
+testTemplate();
+
+//import { createPost } from "./api/posts/post.mjs";
+//import { updatePost } from "./api/posts/put.mjs";
+//import { removePost } from "./api/posts/delete.mjs";
+//import * as post from "./api/posts/index.mjs";
+
+//createPost({
+//	title: "Hello",
+//	body: "Welcome",
+//});
+
+//updatePost({
+//	id: 3742,
+//	title: "Hello once again",
+//	body: "Welcome once more",
+//});
+
+//removePost(3733);
+
 //post.getPosts().then(console.log);
+//post.getPost(3742).then(console.log);
